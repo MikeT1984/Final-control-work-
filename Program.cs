c16b66a0091689b1254ba06e7c0d39013c2da703
Mike <wwwtitov@yandex.ru>
2024-02-21 21:35:07 +0300

    add create array

--- Program.cs
+++ Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Задаем начальный массив строк
+        string[] initialArray = { "Hello", "2", "world", ":-)" };
+        // Формируем новый массив из строк, длина которых меньше или равна 3 символам
+        string[] newArray = CreateNewArray(initialArray);
+        // Выводим новый массив на экран
+        Console.WriteLine("Новый массив:");
+        foreach (string str in newArray)
+        {
+            Console.WriteLine(str);
+        }
+    }
+      static string[] CreateNewArray(string[] array)
+    {
+        // Подсчитываем количество строк в новом массиве
+        int count = 0;
+        foreach (string str in array)
+        {
+            if (str.Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        // Создаем новый массив с известным размером
+        string[] newArray = new string[count];
+
+        // Заполняем новый массив строками, длина которых меньше или равна 3 символам
+        int index = 0;
+        foreach (string str in array)
+        {
+            if (str.Length <= 3)
+            {
+                newArray[index] = str;
+                index++;
+            }
+        }
+
+        return newArray;
+    }
+}
